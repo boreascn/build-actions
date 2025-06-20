6dc08ebce465aa9776941c16029d45fae39d0997
boreascn <42968812+boreascn@users.noreply.github.com>
2025-06-20 20:27:40 +0800

    Update diy-part.sh

--- build/Mt798x/diy-part.sh
+++ build/Mt798x/diy-part.sh
@@ -90,6 +90,8 @@ openwrt-x86-64-generic-kernel.bin
 openwrt-x86-64-generic.manifest
 openwrt-x86-64-generic-squashfs-rootfs.img.gz
 ipk.tar.gz
+openwrt-mediatek-mt7986-xiaomi_redmi-router-ax6000-initramfs-kernel.bin
+openwrt-mediatek-mt7986-xiaomi_redmi-router-ax6000-squashfs-factory.bin
 EOF
 
 # 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci
