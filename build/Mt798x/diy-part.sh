db5065b00ec65de8e5a06aca6dcecf2418d4cf31
boreascn <42968812+boreascn@users.noreply.github.com>
2026-06-03 18:55:12 +0800

    Update diy-part.sh

--- build/Mt798x/diy-part.sh
+++ build/Mt798x/diy-part.sh
@@ -4,10 +4,11 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-# 添加 lucky 插件源
+# 添加插件源
 echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
 echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
 echo 'src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main' >> feeds.conf.default
+sed -i 's/CONFIG_CCACHE=y/# CONFIG_CCACHE is not set/g' .config
 
 # 后台IP设置
 export Ipv4_ipaddr="192.168.16.1"            # 修改openwrt后台地址(填0为关闭)
